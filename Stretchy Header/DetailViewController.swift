908907bb262ac664e494100d530045bcaaaa58c9
Developer <developer@Sawkans-MacBook-Pro.local>
2018-01-08 08:11:56 +0530

    working on blur view over image view

---
+++ Stretchy Header/DetailViewController.swift
@@ -0,0 +1,14 @@
+
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        view.backgroundColor = UIColor.groupTableViewBackground
+       
+    }
+  
+}
--- Stretchy Header/ViewController.swift
+++ Stretchy Header/ViewController.swift
@@ -1,25 +1,73 @@
-//
-//  ViewController.swift
-//  Stretchy Header
-//
-//  Created by Developer on 1/7/18.
-//  Copyright © 2018 Genisys. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.backgroundColor = UIColor.white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.separatorStyle = .none
+        
+        navigationController?.navigationBar.barColor = UIColor.orange
+       
+        tableView.addScalableCover(with: UIImage(named: "images")!, maxHeight: 100)
+        tableView.showsVerticalScrollIndicator = false
+        tableView.tableHeaderView = UIView()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(didTap))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+   override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 66
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        return view
+    }
+    
+    
+    @objc func didTap(){
+         let new = DetailViewController()
+         pushToViewController(new, navigationBarBottomLine: true)
+    }
+    
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.attachToScrollView(tableView)
+    }
+   
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+   
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
     }
 
+    
 
+}
+
+
+extension UIViewController {
+    
+  public func pushToViewController(_ to:UIViewController,navigationBarBottomLine:Bool = false){
+        self.navigationController?.navigationBar.reset()
+        self.navigationController?.pushViewController(to, animated: true)
+        self.navigationController?.navigationBar.updateAlpha(0)
+        if navigationBarBottomLine {
+            self.navigationController?.navigationBar.shadowImage = UIImage()
+        }
+    }
 }
 
